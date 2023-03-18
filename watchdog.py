428e94c7d020551d74d688ee763ff66871ce4f3d
NotoriousPyro <craigcrawford1988@gmail.com>
2023-03-18 13:39:08 +0000

    Convert to appengine

--- watchdog.py
+++ watchdog.py
@@ -1,50 +1,94 @@
 #!/usr/bin/python3 -u
 
+from telegram.client import Telegram
+from google.cloud import datastore, logging as gcloud_logger
+import string
+import random
+import threading
+import time
+import os
+import operator
+import typing as t
+import logging
+
 ##################### Configuration Begin ######################
-YOUR_QUESTION = '12 + 16 = ?'
-YOUR_ANSWER = '28'
-TELEGRAM_API_ID = '11111111' # Get api_id and api_hash at my.telegram.org
-TELEGRAM_API_HASH = '67e72cc9e2b603e08d05446ad5ef8e6'
-TELEGRAM_PHONE = '+12223334444' # Phone number in International Format. Example: '+8617719890604'
+YOUR_QUESTION = os.environ.get("YOUR_QUESTION", "<RANDOM_SUM>")
+YOUR_ANSWER = os.environ.get("YOUR_ANSWER", "<RANDOM_SUM>")
+TELEGRAM_API_ID = os.environ.get("TELEGRAM_API_ID") # Get api_id and api_hash at my.telegram.org
+TELEGRAM_API_HASH = os.environ.get("TELEGRAM_API_HASH")
+TELEGRAM_PHONE = os.environ.get("TELEGRAM_PHONE") # Phone number in International Format. Example: '+8617719890604'
+GAE_PROJECT = os.environ.get("GAE_PROJECT")
 ##################### Configuration End ########################
 
-from telegram.client import Telegram
-import threading, time, os
+logger = gcloud_logger.Client()
+logger.setup_logging()
+
+OPERATORS = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+}
+
+def random_operator():
+    return random.choice(list(OPERATORS.items()))
+
+def random_number(char_num):
+    return int("".join(random.choice(string.digits) for _ in range(char_num)))
+
+if YOUR_QUESTION == "<RANDOM_SUM>":
+    op_string, op_func = random_operator()
+    num_1 = random_number(3)
+    num_2 = random_number(3)
+    YOUR_ANSWER = op_func(num_1, num_2)
+    YOUR_QUESTION = f"{num_1} {op_string} {num_2} = ?"
 
 tg = Telegram(
-    api_id=TELEGRAM_API_ID, 
+    api_id=TELEGRAM_API_ID,
     api_hash=TELEGRAM_API_HASH,
     phone=TELEGRAM_PHONE,
     database_encryption_key='any_password',
     files_directory='tdlib_files/',
 )
 
-whitelist_filename = 'whitelisted_chats.log'
-whitelisted_chat_ids = []
+datastore_client = datastore.Client(project=GAE_PROJECT)
+
+# The kind for the new entity
+kind = "telegram-watchdog"
+# The name/ID for the new entity
+name = "whitelist"
+# The Cloud Datastore key for the new entity
+task_key = datastore_client.key(kind, name)
+
+# Prepares the new entity
+task = datastore_client.get(task_key)
+if task is None:
+    task = datastore.Entity(key=task_key)
+    task["description"] = "Whitelisted Chat IDs"
+    task["chat_ids"] = []
+
+def read_allowlist():
+    return task["chat_ids"]
+
+def write_allowlist():
+    task["chat_ids"] = allowlisted_chat_ids
+    datastore_client.put(task)
+
+def add_to_allowlist(chat_ids: t.List[int]):
+    allowlisted_chat_ids.extend([chat_id for chat_id in chat_ids if chat_id not in allowlisted_chat_ids])
+    write_allowlist()
+
+allowlisted_chat_ids: t.List[int] = read_allowlist()
 
 magic_text = '[tqYH5C]'
-msg_verify = 'This account is protected by Telegram Antispam WatchDog.\nPlease answer the question to continue:\n请正确回答以下问题:\n\n' + YOUR_QUESTION
+msg_verify = 'This account is protected by Telegram Antispam WatchDog.\nPlease answer the question to continue:\n' + YOUR_QUESTION
 msg_whitelisted = '[Telegram Antispam Watchdog] Whitelisted this chat.'
-msg_passed = 'You have passed the verification. Thanks!\n你已经通过验证, 感谢你的理解!'
+msg_passed = 'You have passed the verification. Thanks!\n'
 
 # We need to mark_message_read() for 30 times, with one second interval. That's the only method to eliminate GMS notification.
 # Format: [(chat_id, msg_id, count), ...]
 # count will decrease from 30 to 0 by a timer in another thread.
 remove_gms_notify_queue = []
 remove_gms_notify_queue_lock = threading.Lock()
-
-def read_whitelist_from_disk(fname):
-    try:
-        with open(fname, 'r') as f:
-            for l in f.read().split('\n'):
-                if l != '':
-                    whitelisted_chat_ids.append(int(l))
-    except FileNotFoundError:
-        pass
-
-def write_whitelist_to_disk(fname):
-    with open(fname, 'w+') as f:
-        f.write('\n'.join([str(i) for i in whitelisted_chat_ids]))
 
 def mark_msg_read(chat_id, msg_id):
     # This function must be called multiple times. For example, call it once a second, for 8 times.
@@ -101,30 +145,30 @@ def new_message_handler(update):
 
     if chat_id < 0 or chat_id == 777000:
         return
-    if chat_id in whitelisted_chat_ids:
+    if chat_id in allowlisted_chat_ids:
         return
     if is_outgoing:
         # Send any outgoing message to add unknown chat to whitelist. (Except verification message)
         if magic_text not in message_text:
-            whitelisted_chat_ids.append(chat_id)
-            write_whitelist_to_disk(whitelist_filename)
+            allowlisted_chat_ids.append(chat_id)
+            write_allowlist()
             tg.send_message(chat_id=chat_id, text=msg_whitelisted)
         return
 
-    print("DEBUG: Received a new private chat message which needs verification, chat_id=", chat_id)
+    logging.debug("Received a new private chat message which needs verification, chat_id=", chat_id)
 
     # Mark as read to suppress the notification.
     mark_msg_read(chat_id, msg_id)
 
     if message_content['@type'] == 'messageText' and message_text.lower() == YOUR_ANSWER.lower():
         # Answer is correct: add to whitelist and send hello
-        print("DEBUG: good answer")
-        whitelisted_chat_ids.append(chat_id)
-        write_whitelist_to_disk(whitelist_filename)
+        logging.debug("good answer")
+        allowlisted_chat_ids.append(chat_id)
+        write_allowlist()
         tg.send_message(chat_id=chat_id, text=msg_passed)
     else:
         # Answer is not correct: send verification message and delete his message.
-        print("DEBUG: bad answer")
+        logging.debug("bad answer")
         tg.send_message(chat_id=chat_id, text=magic_text + msg_verify)
         tg.delete_messages(chat_id, [msg_id])
         with remove_gms_notify_queue_lock:
@@ -136,20 +180,18 @@ def timer_thread_func():
         time.sleep(1)
 
 if __name__ == "__main__":
-    read_whitelist_from_disk(whitelist_filename)
     tg.login()
-    
-    # if this is the first run, library needs to preload all chats
-    # otherwise the message will not be sent
     result = tg.get_chats()
     result.wait()
-    print("Started Telegram Antispam Watchdog. API test by listing your chats: ", result.update)
+    current_chat_ids = result.update.get("chat_ids", [])
+    logging.info("Started Telegram Antispam Watchdog. API test by listing your chats: ", current_chat_ids)
+    if not any(allowlisted_chat_ids):
+        logging.info("Adding the following chat IDs as we have an empty whitelist: ", current_chat_ids)
+        add_to_allowlist(current_chat_ids)
 
     threading.Thread(target=timer_thread_func).start()
-
     tg.add_message_handler(new_message_handler)
-    tg.idle()  # blocking waiting for CTRL+C
-    tg.stop()  # you must call `stop` at the end of the script
-    print("Exited")
+    tg.idle()
+    logging.info("Exited")
     os._exit(0)
 
