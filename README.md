5ee33c579bba24bc41c0239d914e82982177c3e4
NotoriousPyro <craigcrawford1988@gmail.com>
2023-03-19 23:31:33 +0000

    Amend to use compute engine

--- README.md
+++ README.md
@@ -1,45 +1,55 @@
 # Telegram AntiSpam Watchdog
 
-Automatically block telegram private message spammer for you!
+Automatically block telegram private message spammers/scammers.
 
-[Chinese Version](README.md)
+## What does it do?
 
-## What does this program do
-
-This is a python script which runs on your VPS / Server 24 hours.
+This is a python script which runs on Google Compute Engine.
 
 It logs into your Telegram account just like a normal telegram client. Once receiving a private message, it will block the notification and delete it, and then send a verification question as reply.
 
-All private messages from the peer will be muted and deleted until he answers the verification question correctly.
+All private messages from the peer will be marked as read and deleted until the question is answered correctly.
 
-Note that this program will whitelist all messages from Group/Channel (with negative chat id) and Telegram official notification (777000). The current chat will be whitelisted if you send any outgoing messages.
+Note that this program will whitelist all messages from Group/Channel (with negative chat id) and Telegram official notification (777000). The current chat will be whitelisted if you send any outgoing messages. The current open chats will also be whitelisted.
+
+These will be saved in Google Datastore.
 
 ## How to install and run
 
-You need a Linux/MacOS device, and then install **python3 and python-telegram** on it. For example, if you are using ubuntu, you should run this:
-
-```
-sudo apt install python3 python3-pip
-sudo pip3 install python-telegram
-```
-
-And then copy the `watchdog.py` onto your device. Modify the following lines at the top of the file. Refer to [Telegram Official Document](https://core.telegram.org/api/obtaining_api_id#obtaining-api-id) about how to get `api_id` and `api_hash`.
-
-```
-YOUR_QUESTION = 'Your verification question here'
-YOUR_ANSWER = 'Your expected answer here'
-TELEGRAM_API_ID = 'Change This!'
-TELEGRAM_API_HASH = 'Change This!'
-TELEGRAM_PHONE = 'Phone number of your telegram account'
-```
-
-Now you just need to run the modified `watchdog.py` 24 hours a day. Note that you need to input the SMS code on the first login.
-
-## Docker
-
-TODO
-
-## FAQ
-
-No question yet.
-
+1. Sign up to Google Cloud
+2. Enable the following APIs:
+    * Cloud Datastore API
+    * Cloud Logging API
+    * Compute Engine API
+3. Go to Compute Engine and set up a VM, ec2-micro should suffice and is within the free tier. Make sure to give scope access for:
+    * Datastore read/write
+    * Logging write
+4. SSH to the VM and install `git`, `python3-pip`:
+    ```bash
+    sudo apt install git python3-pip
+    ```
+5. Clone:
+    ```bash
+    git clone https://github.com/NotoriousPyro/telegram-antispam.git
+    ```
+6. Install prereqs:
+    ```bash
+    python3 -m pip install -r requirements.txt
+    ```
+7. Copy `config.env.example` to `config.env` and edit the parameters:
+    * `GCP_PROJECT_ID` - this is visible in the URL of your gcloud project.
+    * `TELEGRAM_API_HASH` and `TELEGRAM_API_ID` are obtainable from [obtaining-api-id](https://core.telegram.org/api/obtaining_api_id#obtaining-api-id).
+    * `TELEGRAM_DB_PASSWORD` should be a password, preferably using a password generator.
+    * `TELEGRAM_PHONE` should be the phone number associated to your Telegram account.
+8. (first run) Do the following to run it interactively to start, as you'll need to input a PIN (check Telegram):
+    1. Copy the lines from `config.env` and add `export` to the start of them, and then run them.
+    2. Run `python3 -m main.py`
+    3. Enter the PIN in Telegram into the console window.
+    4. Once confirmed it is running without errors, close it with `CTRL + C`
+9. Copy the included `telegram-antispam.service` to `/etc/systemd/system` and edit the paths (`ExecStart` and `EnvironmentFile`) pointing to where `main.py` and `config.env` are also change `User` to the desired user account the service should run as.
+10. Enable and run the service:
+    ```
+    sudo systemctl enable telegram-antispam
+    sudo systemctl start telegram-antispam
+    ```
+11. View the logs in gcloud and using `journalctl -f -u telegram-antispam` on the VM, should be no errors.
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3 -u
 
-from telegram.client import Telegram
 from google.cloud import datastore, logging as gcloud_logger
+from telegram.client import Telegram
+
+import logging
+import os
+import operator
 import string
 import random
 import threading
 import time
-import os
-import operator
 import typing as t
-import logging
 
 ##################### Configuration Begin ######################
 YOUR_QUESTION = os.environ.get("YOUR_QUESTION", "<RANDOM_SUM>")
@@ -18,7 +19,7 @@ TELEGRAM_DB_PASSWORD = os.environ.get("TELEGRAM_DB_PASSWORD")
 TELEGRAM_API_ID = os.environ.get("TELEGRAM_API_ID") # Get api_id and api_hash at my.telegram.org
 TELEGRAM_API_HASH = os.environ.get("TELEGRAM_API_HASH")
 TELEGRAM_PHONE = os.environ.get("TELEGRAM_PHONE") # Phone number in International Format. Example: '+8617719890604'
-GAE_PROJECT = os.environ.get("GAE_PROJECT")
+GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 ##################### Configuration End ########################
 
 logger = gcloud_logger.Client()
@@ -36,11 +37,12 @@ def random_operator():
 def random_number(char_num):
     return int("".join(random.choice(string.digits) for _ in range(char_num)))
 
+# Only generates a random equation on startup, need to make it generate a new one per chat id
 if YOUR_QUESTION == "<RANDOM_SUM>":
     op_string, op_func = random_operator()
     num_1 = random_number(3)
     num_2 = random_number(3)
-    YOUR_ANSWER = op_func(num_1, num_2)
+    YOUR_ANSWER = str(op_func(num_1, num_2))
     YOUR_QUESTION = f"{num_1} {op_string} {num_2} = ?"
 
 tg = Telegram(
@@ -51,24 +53,21 @@ tg = Telegram(
     files_directory='/tmp/tdlib_files/',
 )
 
-datastore_client = datastore.Client(project=GAE_PROJECT)
-
-# The kind for the new entity
-kind = "telegram-watchdog"
-# The name/ID for the new entity
-name = "whitelist"
-# The Cloud Datastore key for the new entity
-task_key = datastore_client.key(kind, name)
+datastore_client = datastore.Client(project=GCP_PROJECT_ID)
+task_key = datastore_client.key("telegram-watchdog", "whitelist")
 
 # Prepares the new entity
 task = datastore_client.get(task_key)
 if task is None:
     task = datastore.Entity(key=task_key)
-    task["description"] = "Whitelisted Chat IDs"
+    task["description"] = "Allowlisted Chat IDs"
     task["chat_ids"] = []
 
 def read_allowlist():
-    return task["chat_ids"]
+    chat_ids = task["chat_ids"]
+    if any(chat_ids):
+        logging.info("Loaded the following saved chat IDs: %s", chat_ids)
+    return chat_ids
 
 def write_allowlist():
     task["chat_ids"] = allowlisted_chat_ids
@@ -80,9 +79,9 @@ def add_to_allowlist(chat_ids: t.List[int]):
 
 allowlisted_chat_ids: t.List[int] = read_allowlist()
 
-magic_text = '[tqYH5C]'
+magic_text = '[Watchdog]'
 msg_verify = 'This account is protected by Telegram Antispam WatchDog.\nPlease answer the question to continue:\n' + YOUR_QUESTION
-msg_whitelisted = '[Telegram Antispam Watchdog] Whitelisted this chat.'
+msg_allowlisted = 'Allowlisted this chat.'
 msg_passed = 'You have passed the verification. Thanks!\n'
 
 # We need to mark_message_read() for 30 times, with one second interval. That's the only method to eliminate GMS notification.
@@ -130,7 +129,7 @@ def timer_handler():
                 mark_msg_read_finish(chat_id)
         remove_gms_notify_queue = result_list
 
-def new_message_handler(update):
+def message_handler(update):
     chat_id = update['message']['chat_id']
     msg_id = update['message']['id']
     message_content = update['message']['content']
@@ -140,36 +139,36 @@ def new_message_handler(update):
     # This handler will block all message which satisfies ALL of the following condition:
     # 1. Incoming
     # 2. Not from group chat (Personal chat)
-    # 3. chat_id is not in whitelist
+    # 3. chat_id is not in allowlist
     # 4. chat_id is not 777000 (Telegram official notification)
-    # Maybe we can whitelist sender_id instead of chat_id, but I think it doesn't make a difference.
+    # Maybe we can allowlist sender_id instead of chat_id, but I think it doesn't make a difference.
 
     if chat_id < 0 or chat_id == 777000:
         return
     if chat_id in allowlisted_chat_ids:
         return
     if is_outgoing:
-        # Send any outgoing message to add unknown chat to whitelist. (Except verification message)
-        if magic_text not in message_text:
-            allowlisted_chat_ids.append(chat_id)
-            write_allowlist()
-            tg.send_message(chat_id=chat_id, text=msg_whitelisted)
+        # Send any outgoing message to add unknown chat to allowlist. (Except verification message)
+        if magic_text in message_text:
+            return
+        allowlisted_chat_ids.append(chat_id)
+        write_allowlist()
+        tg.send_message(chat_id=chat_id, text=magic_text + msg_allowlisted)
         return
 
-    logging.info("Received a new private chat message which needs verification, chat_id=", chat_id)
-
+    logging.info("Received a new private chat message which needs verification, chat_id=%s", chat_id)
     # Mark as read to suppress the notification.
     mark_msg_read(chat_id, msg_id)
 
     if message_content['@type'] == 'messageText' and message_text.lower() == YOUR_ANSWER.lower():
-        # Answer is correct: add to whitelist and send hello
+        # Answer is correct: add to allowlist and send hello
         logging.debug("good answer")
         allowlisted_chat_ids.append(chat_id)
         write_allowlist()
-        tg.send_message(chat_id=chat_id, text=msg_passed)
+        tg.send_message(chat_id=chat_id, text=magic_text + msg_passed)
     else:
         # Answer is not correct: send verification message and delete his message.
-        logging.debug("bad answer")
+        logging.debug("bad answer: %s, expected: %s", message_text.lower(), YOUR_ANSWER.lower())
         tg.send_message(chat_id=chat_id, text=magic_text + msg_verify)
         tg.delete_messages(chat_id, [msg_id])
         with remove_gms_notify_queue_lock:
@@ -185,14 +184,25 @@ if __name__ == "__main__":
     result = tg.get_chats()
     result.wait()
     current_chat_ids = result.update.get("chat_ids", [])
-    logging.info("Started Telegram Antispam Watchdog. API test by listing your chats: ", current_chat_ids)
+    logging.info("Started Telegram Antispam Watchdog. API test by listing your chats: %s", current_chat_ids)
     if not any(allowlisted_chat_ids):
-        logging.info("Adding the following chat IDs as we have an empty whitelist: ", current_chat_ids)
+        logging.info("Adding the following chat IDs as we have an empty allowlist: %s", current_chat_ids)
         add_to_allowlist(current_chat_ids)
 
+    missed_new_chats = [chat_id for chat_id in current_chat_ids if chat_id not in allowlisted_chat_ids]
+    # If there are any new messages while the application was not running, let's challenge them.
+    if any(missed_new_chats):
+        messages = []
+        for chat_id in missed_new_chats:
+            messages_res = tg.get_chat_history(chat_id, limit=1)
+            messages_res.wait()
+            messages.append(messages_res.update.get("messages"))
+        if any(messages):
+            logging.info("Will challenge for the following missed messages: %s", messages)
+            [message_handler({"message": message[0]}) for message in messages]
+
     threading.Thread(target=timer_thread_func).start()
-    tg.add_message_handler(new_message_handler)
+    tg.add_message_handler(message_handler)
     tg.idle()
     logging.info("Exited")
     os._exit(0)
-
